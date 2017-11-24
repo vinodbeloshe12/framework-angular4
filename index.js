9c98fb70e958a41a2c053c12e57a3abc22204b9d
Vinod Beloshe <vinodb@qgold.com>
2017-11-24 15:27:22 +0530

    code

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ var curPath = process.cwd();
 var folderName = "framework-angular4";
 var source = "https://github.com/vinodbeloshe12/angular";
 var destination = path.join(curPath, folderName);
-console.log(curPath, "dest", destination);
-
 // download();
 
 module.exports = download
 
 function download() {
-  console.log("in download");
   mkdirp(destination, function (err) {
     if (err) {
       console.error(err);
