ec697fdd9ab5b82b02c593e598c07740702bbea2
Vinod Beloshe <vinodb@qgold.com>
2017-11-24 11:50:52 +0530

    changes

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var clone = require('git-clone');
 module.exports = download
 
 function download(callback) {
-  clone('https://github.com/vinodbeloshe12/angular', 'test', callback);
+  clone('https://github.com/vinodbeloshe12/angular', '../../test', callback);
 }
