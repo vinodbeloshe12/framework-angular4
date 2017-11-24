b1bec7981dabafdc9a3c366f4c83aa6d2475b9bc
Vinod Beloshe <vinodb@qgold.com>
2017-11-24 12:19:18 +0530

    changes mkdir

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var clone = require('git-clone');
+var mkdirp = require('mkdirp');
+var path = require('path');
+
+console.log("Going to create directory /tmp/test");
 
 
 module.exports = download
 
 function download(callback) {
-  clone('https://github.com/vinodbeloshe12/angular', '../../test', callback);
+  mkdirp(path.join(process.cwd(), '/framework-angular4'), function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    console.log("Directory created successfully!");
+    clone('https://github.com/vinodbeloshe12/angular', '/framework-angular4', callback);
+  });
+
 }
